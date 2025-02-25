7f59119b144528d9c00c84714e7bf2b0084e6325
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-02-26 01:22:56 +0530

    Mini svg installed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,6 +9,8 @@ const Hero = () => {
                 <Spotlight className="top-10 left-full h-[80vh] w-[50vw]" fill="purple" />
                 <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
             </div>
+
+            
         </div>
     )
 }
