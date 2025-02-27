ccd161c790ccd309867dff98323b0da74cd666e3
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-02-27 20:28:01 +0530

    Working on Hero Section - Hero Text

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,5 @@
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 const Hero = () => {
   return (
@@ -23,6 +24,10 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">Dyanamic Web Magic With Next.js</h2>
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words="Transforming Concepts into Seamless User Experiences"
+          />
         </div>
       </div>
     </div>
