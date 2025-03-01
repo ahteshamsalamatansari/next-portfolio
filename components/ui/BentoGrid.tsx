9a0728cb1758e4e1026d31744618a4f6153a9d32
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-03-01 13:44:14 +0530

    Working on Grid Section

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -50,6 +50,12 @@ export const BentoGridItem = ({
           "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
       }}
     >
+
+      <div className={`${id===6 }&& 'flex justify-center h-full'`}>
+        <div>
+          
+        </div>
+      </div>
       
       <div className="group-hover/bento:translate-x-2 transition duration-200">
        
