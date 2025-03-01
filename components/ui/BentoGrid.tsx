94da044696c2314f1af6cf14be9c5db9f0a18b81
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-03-01 17:10:55 +0530

    Working on Grid Section

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -97,18 +97,22 @@ export const BentoGridItem = ({
             {title}
           </div>
         </div>
-          {id === 2 && <GridGlobe/>}
+        {id === 2 && <GridGlobe />}
 
-          {id === 3 && (
-            <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2"> 
-                <div className="flex flex-col gap-3 lg:gap-8 ">
-                    {leftLists.map((item)=>(
-                      
-                    ))}
-                </div>
+        {id === 3 && (
+          <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+            <div className="flex flex-col gap-3 lg:gap-8 ">
+              {leftLists.map((item) => (
+                <span
+                  key={item}
+                  className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 text-center bg-[#10132e]"
+                >
+                  {item}
+                </span>
+              ))}
             </div>
-          )}
-
+          </div>
+        )}
       </div>
     </div>
   );
