7f94e4d715d027c4142f216200618eef923f3ee0
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-03-01 12:14:21 +0530

    Hero Completed ♥

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -26,7 +26,7 @@ const Hero = () => {
 
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">Dyanamic Web Magic With Next.js</h2>
+          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80"> Dynamic Web Magic With Next.js </h2>
           <TextGenerateEffect
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
             words="Transforming Concepts into Seamless User Experiences"
