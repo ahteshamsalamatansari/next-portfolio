6b748c8464282b2fc86e7f10181bc39ce72040f2
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-03-01 17:37:02 +0530

    Working on Grid Section

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -101,11 +101,26 @@ export const BentoGridItem = ({
 
         {id === 3 && (
           <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
-            <div className="flex flex-col gap-3 lg:gap-8 ">
-              {leftLists.map((item) => (
+            {/* tech stack lists */}
+            <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
+              {leftLists.map((item, i) => (
                 <span
-                  key={item}
-                  className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 text-center bg-[#10132e]"
+                  key={i}
+                  className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+                    lg:opacity-100 rounded-lg text-center text-white-100 bg-[#10132E]"
+                >
+                  {item}
+                </span>
+              ))}
+              <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-white-100 text-center bg-[#10132E]"></span>
+            </div>
+            <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
+              <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-white-100 text-center bg-[#10132E]"></span>
+              {rightLists.map((item, i) => (
+                <span
+                  key={i}
+                  className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:base opacity-50 text-white-100
+                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
                 >
                   {item}
                 </span>
